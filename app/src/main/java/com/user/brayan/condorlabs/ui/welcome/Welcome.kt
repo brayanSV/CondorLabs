00552fa25552b2ac3cb4ddfe0c62fe233c111277
brayanSV <villarragabrayan@hotmail.com>
2021-10-24 11:43:44 -0500

    Se finaliza la prueba tecnica, se agrega la pantalla de bienvenidos

--- app/src/main/java/com/user/brayan/condorlabs/ui/welcome/Welcome.kt
+++ app/src/main/java/com/user/brayan/condorlabs/ui/welcome/Welcome.kt
@@ -1,14 +1,30 @@
 package com.user.brayan.condorlabs.ui.welcome
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.user.brayan.condorlabs.R
+import com.user.brayan.condorlabs.ui.main.MainActivity
+
 
 class Welcome : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_welcome)
     }
 
+    override fun onResume() {
+        super.onResume()
 
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this@Welcome, MainActivity::class.java)
+                this@Welcome.startActivity(intent)
+                finish()
+            }, 2000
+        )
+    }
 }
